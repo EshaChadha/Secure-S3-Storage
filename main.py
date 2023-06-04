3fbf28997805383d38fd7ce47df807b79cb5a5e8
EshaChadha <54662098+EshaChadha@users.noreply.github.com>
2023-06-04 18:28:12 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import uploadS3
 import os
 import boto3
 import decrypt
+import check
 
 def main():
     bucket = None
@@ -17,17 +18,13 @@ def main():
         opts, args = getopt.getopt(argv, "b:o:i:m:h:")
 
     except:
-        print ("usage: python3 main.py -m upload -b <bucket-name> -o <object> -i <image-name>")
-        print ("usage: python3 main.py -m download -b <bucket-name> -o <object> -i <image-name>")
-        print("usage: python3 main.py -m decrypt -i <image-name>")
+        help_msg()
         sys.exit(2)
 
     else:
         for opt, arg in opts:
             if opt in ['-h']:
-                print ("usage: python3 main.py -m upload -b <bucket-name> -o <object> -i <image-name>")
-                print ("usage: python3 main.py -m download -b <bucket-name> -o <object> -i <image-name>")
-                print ("usage: python3 main.py -m decrypt -i <image-name>")
+                help_msg()        
                 os._exit(0)
 
             elif opt in ['-m']:
@@ -41,15 +38,45 @@ def main():
         if (mode == "upload"):
             hybrid.mainMenu(src)
             uploadS3.upload_file(src, bucket, object)
-            print("Uploaded Successfully!!!")
+            print("Uploaded Successfully!")
 
         elif (mode == "download"):
             s3 = boto3.client('s3')
             s3.download_file(bucket, object, src)
-            print("File Downloaded!!!")
+            print("File Downloaded!")
 
         elif (mode == "decrypt"):
             decrypt.main(src)
+
+        elif (mode == "check"):
+            check.main()
+
+class Color:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+def print_heading(heading):
+    print(Color.BOLD + heading + Color.END)
+
+def help_msg():
+    print("\t\t----------------------")
+    print(Color.BLUE + "\t\t\tWelcome!!" + Color.END)
+    print(Color.BLUE + "\t\tSecure S3 Data Storage" + Color.END)
+    print("\t\t----------------------\n")
+    print_heading("Check for security issues in S3")
+    print("py main.py -m check\n")
+    print_heading("Enter text and upload image")
+    print("py main.py -m upload -b <bucket-name> -o <object> -i <image-name>\n")
+    print_heading("Download an object")
+    print("py main.py -m download -b <bucket-name> -o <object> -i <image-name>\n")
+    print_heading("Decrypt Image")
+    print("py main.py -m decrypt -i <image-name>\n")
+
 
 if __name__ == '__main__':
     main()
